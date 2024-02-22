7b349f05ca225d7f0321ecd9d867b85cea39d2de
Lara Martin <lara@campusevolve.ai>
2024-02-22 09:31:00 -0800

    Add middleware and filter to add logging and headers for request context

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -22,5 +22,6 @@ log_file = "-"
 bind = "0.0.0.0:8000"
 
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = (multiprocessing.cpu_count() * 2) + 1
+#workers = (multiprocessing.cpu_count() * 2) + 1
+workers = 2
 
