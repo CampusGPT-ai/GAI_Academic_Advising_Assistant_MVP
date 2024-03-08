101a78a6c2e8cda65d00fc84db65d5f1cb22e91e
Lara Martin <lara@campusevolve.ai>
2024-03-07 17:23:07 -0800

    For now, only fail tests with critical accessibility impact

--- client/.storybook/test-runner.js
+++ client/.storybook/test-runner.js
@@ -1,3 +1,4 @@
+const { getStoryContext } = require('@storybook/test-runner');
 const { injectAxe, checkA11y } = require('axe-playwright');
 
 /*
@@ -8,8 +9,15 @@ module.exports = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page) {
+  async postVisit(page, context) {
+    const storyContext = await getStoryContext(page, context);
+
+    // Do not run a11y tests on disabled stories.
+    if (storyContext.parameters?.a11y?.disable) {
+      return;
+    }
     await checkA11y(page, '#storybook-root', {
+      includedImpacts: ['critical'],
       detailedReport: true,
       detailedReportOptions: {
         html: true,
