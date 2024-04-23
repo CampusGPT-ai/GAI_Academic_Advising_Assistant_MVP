136475e1aa2d559a743d00cecfa185c59d5c8806
marycampus <mary@prosocial.care>
2024-04-23 12:42:55 -0700

    add fix to open links in new window

--- client/src/components/chat/chatElements/chatMessageElements/chatResponse/chatBotChatResponse.tsx
+++ client/src/components/chat/chatElements/chatMessageElements/chatResponse/chatBotChatResponse.tsx
@@ -15,6 +15,21 @@ const ChatBotChatResponse: FC<ChatBotChatResponseProps> = ({
   message,
 }) => {
   const messageWithLineBreaks = message ? message.replace(/\n/g, '<br>') : "";
+  // Add a hook to make all links open a new window
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+  // set all elements owning target to target=_blank
+  if ('target' in node) {
+    node.setAttribute('target', '_blank');
+  }
+  // set non-HTML/MathML links to xlink:show=new
+  if (
+    !node.hasAttribute('target') &&
+    (node.hasAttribute('xlink:href') || node.hasAttribute('href'))
+  ) {
+    node.setAttribute('xlink:show', 'new');
+  }
+});
+
   const sanitizedHTML = DOMPurify.sanitize(messageWithLineBreaks, { USE_PROFILES: { html: true } });
 
   // Use a div and dangerouslySetInnerHTML to render sanitized HTML
