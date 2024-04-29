7980e47d7e36d5d8db1285a14426280f52161546
marycampus <mary@prosocial.care>
2024-04-29 11:33:54 -0700

    front end err handling

--- client/src/api/validateToken.ts
+++ client/src/api/validateToken.ts
@@ -29,45 +29,61 @@ const sendTokenToBackend = async( {accounts, isAuthenticated, inProgress, instan
                 const account = accounts[0];
         
                 if ( isAuthenticated && inProgress === 'none' && instance) {
+                    let response;
+                    try {
+                        response = await instance.acquireTokenSilent({
+                            scopes: ['828d30c9-9619-4a12-8604-96d44653958f/.default'], // Use .default alone
+                            account: account,
+                        });
+                    } catch (error) {
+                        console.error('Silent token acquisition failed.');
+                        throw error;
+                    }
 
-                    const response = await instance.acquireTokenSilent({
-                        scopes: ['828d30c9-9619-4a12-8604-96d44653958f/.default'], // Use .default alone
-                        account: account,
-                    });
-                    
-        
                     const token: string = response.accessToken;
-
-                    backendResponse = await fetch(`${BaseUrl()}/validate_token`, {
+                    try {
+                        backendResponse = await fetch(`${BaseUrl()}/validate_token`, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
                             'Authorization': `Bearer ${token}`
                         },
-    
-                });
-            };
-            }
-            else {
-                backendResponse = await fetch(`${BaseUrl()}/get_generic_token`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-            }
+                    });
+                    } catch (error) {
+                        console.error('Token validation failed');
+                        throw error;
+                    }
+                }
+                else {
+                    return;
+                }
+        }
+        else {
+            backendResponse = await fetch(`${BaseUrl()}/get_generic_token`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
             
             // console.log(`Backend response recieved from server: ${backendResponse}`)
-            if (!backendResponse.ok) {
+            if (backendResponse && !backendResponse.ok) {
                 // Handle the scenario where the token is not valid
                 console.error('Token validation failed');
                 // Handle error, redirect to login or show an error message
             } else {
                 // Token is valid, user is considered logged in on the server
                 // console.log('User logged in successfully');
-                const data: BackendResponse = await backendResponse.json();
+                if (backendResponse)
+                {const data: BackendResponse = await backendResponse.json();
                 // console.log(`Received user data from backend response ${JSON.stringify(data)}`)
                 return data.session_id
+                }
+                else {
+                    console.error('No data received from backend response');
+                    throw new Error('No data received from backend response');
+                }
                 // You can perform further actions here if needed
             }
         } catch (error: any) {
