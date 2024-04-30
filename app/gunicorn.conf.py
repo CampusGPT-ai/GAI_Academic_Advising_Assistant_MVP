af3004ec835555408498d65e9272627ef1a2876a
marycampus <mary@prosocial.care>
2024-04-30 10:06:11 -0700

    just environment variables

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -6,15 +6,15 @@ import os
 max_requests = 1000
 max_requests_jitter = 50
 
-with open(os.getenv('LOG_CONFIG_FILE', 'logging.yaml'), 'r') as f:
-    log_config = yaml.safe_load(f)
+#with open(os.getenv('LOG_CONFIG_FILE', 'logging.yaml'), 'r') as f:
+ #   log_config = yaml.safe_load(f)
 
-logconfig_dict = log_config
+#logconfig_dict = log_config
 
-logconfig_dict['root'] = {
-    'level': 'WARNING',
-    'handlers': ['default'],
-}
+#logconfig_dict['root'] = {
+ #   'level': 'WARNING',
+ #   'handlers': ['default'],
+#}
 
 
 log_file = "-"
