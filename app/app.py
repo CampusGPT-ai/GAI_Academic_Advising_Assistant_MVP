e1c1de2411800e3359c408c460cbd28ac384bece
marycampus <143058217+MaryProSocial@users.noreply.github.com>
2024-08-23 10:05:31 -0700

    Revert "push query"
    This reverts commit 6cf94e73c2cdec4363264a8b5bac60bc66526bd2.

--- app/app.py
+++ app/app.py
@@ -273,24 +273,6 @@ async def chat_new(
             content={"message": f"failed to return chat response with error {str(e)}"},
             status_code=404,
         )
-    
-@app.get("/research-count")
-async def get_research_count(
-    session_data: UserSession = Depends(get_session_from_session),
-):
-    try:
-        count = get_research_count(session_data)
-        logger.info(f'found research count: {count}')
-        return JSONResponse(content={"data": count}, status_code=200)
-    except Exception as e:
-        logger.error(
-            f"failed to load research count with error {str(e)}",
-        )
-        message_content = {"message": f"failed to load research count with error {str(e)}"}
-        return JSONResponse(
-            content=message_content,
-            status_code=404,
-        )
 
 @app.get("/users/{session_guid}/outcomes/{conversation_id}")
 async def get_outcomes(
