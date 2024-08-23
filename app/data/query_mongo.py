b9e0a5fa63b8a508925d321b6d308b0c2b1791a0
marycampus <143058217+MaryProSocial@users.noreply.github.com>
2024-08-23 10:08:15 -0700

    merge commit

--- app/data/query_mongo.py
+++ app/data/query_mongo.py
@@ -15,6 +15,60 @@ def format_datetime(dt):
         return dt.strftime('%Y-%m-%d %H:%M:%S')
     return ''
 
+def load_data_to_dataframe():
+    profiles_data = []
+    conversations_data = []
+
+    # Fetch all objects in the collection and store them in a list
+    all_conversations = list(ConversationSimple.objects.select_related(max_depth=2))
+    all_profiles = list(Profile.objects)
+    all_messages = list(RawChatMessage.objects.select_related(max_depth=2))
+
+    # Fetch all conversations and corresponding profiles
+    for conversation in all_conversations:
+        profile = [profile for profile in all_profiles if profile.user_id == conversation.user_id]
+        if not profile:
+            continue
+        if profile[0].user_id in ['mary.schwaber@gmail.com']:
+            continue
+        messages = [raw_message_ref.message for raw_message_ref in conversation.history]
+        for raw_message in messages:
+            for msg_content in raw_message:
+                conversation_data = {
+                    'user_id': conversation.user_id,
+                    'email': profile[0].email,
+                    'q1_usefullness_scale': msg_content.feedback.get("q1_usefullness", msg_content.feedback.get("usefulness")).get("scale") if (msg_content.feedback != {} and msg_content.feedback is not None) else '',
+                    'q2_relevancy_scale': msg_content.feedback.get("q2_relevancy", msg_content.feedback.get("relevance")).get("scale") if (msg_content.feedback != {} and msg_content.feedback is not None) else '',
+                    'q3_accuracy_scale': msg_content.feedback.get("q3_accuracy", msg_content.feedback.get("accuracy")).get("scale") if (msg_content.feedback != {} and msg_content.feedback is not None) else '',
+                    'helpfulnessofLinks': msg_content.feedback.get("helpfulnessOfLinks").get("scale") if (msg_content.feedback != {} and msg_content.feedback is not None and msg_content.feedback.get("helpfulnessOfLinks")) else '',
+                    'learnMoreOptions': msg_content.feedback.get("learnMoreOptions").get("scale") if (msg_content.feedback != {} and msg_content.feedback is not None and msg_content.feedback.get("learnMoreOptions")) else ''
+                }
+                conversations_data.append(conversation_data)
+    return pd.DataFrame(conversations_data)
+
+def count_reviews(df):
+    # Drop rows where any feedback field is NaN
+    df.replace('', np.nan, inplace=True)
+    df_clean = df.dropna(subset=['q1_usefullness_scale', 'q2_relevancy_scale', 'q3_accuracy_scale', 'helpfulnessofLinks', 'learnMoreOptions'])
+    df_clean.to_csv('conversations_data.csv')
+    print(df_clean.head())
+    print(df_clean.describe())
+    # Group by user_id and count the number of reviews per user
+    user_review_counts = df_clean.groupby('email').size()
+    user_review_counts.to_csv('user_review_counts.csv')
+
+    
+    # Count users with at least one completed review
+    users_with_at_least_one_review = user_review_counts[user_review_counts >= 1].count()
+    
+    # Count users with at least five completed reviews
+    users_with_five_reviews = user_review_counts[user_review_counts >= 5].count()
+    
+    return {
+        'total_users_with_one_review': users_with_at_least_one_review,
+        'total_users_with_five_reviews': users_with_five_reviews
+    }
+
 def export_messages_to_csv(filename):
     # Open a CSV file for writingmarynelson@Marys-Laptop GAI_Academic_Advising_Assistant_MVP 
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
